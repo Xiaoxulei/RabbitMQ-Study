651219a5c02e972413b0456ec48bc80e8a4781cc
xuxiaolei <xulei_wh@outlook.com>
2025-08-11 11:16:46 +0800

    first commit

---
+++ rabbitmq-study-demo/src/main/java/com/xuxiaolei/RabbitMqDemoMainApplication.java
@@ -0,0 +1,16 @@
+package com.xuxiaolei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: xuxiaolei
+ * @Description: TODO:
+ * @CreatTime: 2025/08/11 10:16
+ **/
+@SpringBootApplication
+public class RabbitMqDemoMainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMqDemoMainApplication.class, args);
+    }
+}
